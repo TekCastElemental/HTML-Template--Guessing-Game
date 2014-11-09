6a9b6edb24c80fe5e7280bbdb417ab442cf72e9c
TekCastElemental <Max560gaming@gmail.com>
2014-11-09 10:46:37 -0600

    SOURCE

---
+++ GameApplet.java
@@ -0,0 +1,13 @@
+package HTMLGG;
+
+import javax.swing.JApplet;
+
+public class GameApplet extends JApplet {
+	private static final long serialVersionUID = 1l;
+
+	public void init() {
+		setContentPane(new GamePanel());
+	}
+	
+	
+}
